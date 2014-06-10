fc25b6201a4075b5f2da6c92744820c63f54b763
mars <constantine.mars@gmail.com>
2014-06-10 18:32:23 +0300

    GCM example: registerInBackground

--- cloudmessaginglearn/CloudMessagingLearn/src/c/mars/cloudmessaginglearn/MainActivity.java
+++ cloudmessaginglearn/CloudMessagingLearn/src/c/mars/cloudmessaginglearn/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends ActionBarActivity {
             regid = getRegistrationId(context);
 
             if (regid.isEmpty()) {
-//                TODO: registerInBackground();
+                registerInBackground();
             }
         } else {
         	Log.i(TAG, "No valid Google Play Services APK found.");
@@ -215,7 +215,46 @@ public class MainActivity extends ActionBarActivity {
        editor.putInt(PROPERTY_APP_VERSION, appVersion);
        editor.commit();
    }
-   
+   private void registerInBackground() {
+	    new AsyncTask<Void, Integer, String>() {
+	        @Override
+	        protected String doInBackground(Void... params) {
+	            String msg = "";
+	            try {
+	                if (gcm == null) {
+	                    gcm = GoogleCloudMessaging.getInstance(context);
+	                }
+	                regid = gcm.register(SENDER_ID);
+	                msg = "Device registered, registration ID=" + regid;
+
+	                // You should send the registration ID to your server over HTTP,
+	                // so it can use GCM/HTTP or CCS to send messages to your app.
+	                // The request to your server should be authenticated if your app
+	                // is using accounts.
+	                sendRegistrationIdToBackend();
+
+	                // For this demo: we don't need to send it because the device
+	                // will send upstream messages to a server that echo back the
+	                // message using the 'from' address in the message.
+
+	                // Persist the regID - no need to register again.
+	                storeRegistrationId(context, regid);
+	            } catch (IOException ex) {
+	                msg = "Error :" + ex.getMessage();
+	                // If there is an error, don't just keep trying to register.
+	                // Require the user to click a button again, or perform
+	                // exponential back-off.
+	            }
+	            return msg;
+	        }
+
+	        @Override
+	        protected void onPostExecute(String msg) {
+	            display.append(msg + "\n");
+	        }
+	    }.execute(null, null, null);
+	 
+	}   
    public void onClick(final View view) {
 	    if (view == findViewById(R.id.send)) {
 	        new AsyncTask<Void, Integer, String>() {
