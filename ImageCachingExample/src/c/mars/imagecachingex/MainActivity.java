f8a1835cd890aa2aa9bd5b08365797c4fce87701
mars <constantine.mars@gmail.com>
2014-06-16 18:18:09 +0300

    Simple gallery

--- ImageCachingExample/src/c/mars/imagecachingex/MainActivity.java
+++ ImageCachingExample/src/c/mars/imagecachingex/MainActivity.java
@@ -3,33 +3,35 @@ package c.mars.imagecachingex;
 import java.io.IOException;
 import java.io.InputStream;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.content.Intent;
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Gallery;
 import android.widget.ImageView;
-import android.os.Build;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
 	final static String landscapesFolder = "landscapes";
 	static String[] files;
 	static AssetManager assets;
+	
 	ImageView imageView;  
-	@SuppressWarnings("deprecation")
-	Gallery gallery;
-
+	LinearLayout gallery;
+	ImageAdapter adapter;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,29 +39,69 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         
         imageView = (ImageView)findViewById(R.id.imageView);
-        gallery = (Gallery)findViewById(R.id.gallery);
+        gallery = (LinearLayout)findViewById(R.id.gallery);
+        adapter = new ImageAdapter(this);
         
         assets = getAssets();
         try {
 			files = assets.list(landscapesFolder);
-		
+		int i = 0;
             for (String file : files) {
-				String fn = landscapesFolder + "/" +  file;
-				InputStream stream;
-				stream = assets.open(landscapesFolder + "/" +  file);
-
-				Drawable drawable = Drawable.createFromStream(stream, null);
-				if (drawable != null) {
-					imageView.setImageDrawable(drawable);
-					return;
-				}
+            	loadImage(file);
+//            	return;
             }
 				
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        
+//        String ExternalStorageDirectoryPath = Environment
+//                .getExternalStorageDirectory()
+//                .getAbsolutePath();
+//              
+//              String targetPath = ExternalStorageDirectoryPath + "/test/";
+//              Toast.makeText(getApplicationContext(), targetPath, Toast.LENGTH_LONG).show();
+              
     }
+    
+    void loadImage(String file) throws IOException {
+    	try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	String fn = landscapesFolder + "/" +  file;
+		InputStream stream;
+		stream = assets.open(landscapesFolder + "/" +  file);
+
+		Drawable drawable = Drawable.createFromStream(stream, null);
+		if (drawable != null) {
+//			imageView.setImageDrawable(drawable);	
+			gallery.addView(insertPhoto(drawable));
+		}
+
+    }
+    
+    View insertPhoto(Drawable drawable){
+//        Bitmap bm = decodeSampledBitmapFromUri(path, 220, 220);
+        
+        LinearLayout layout = new LinearLayout(getApplicationContext());
+        layout.setLayoutParams(new LayoutParams(250, 250));
+        layout.setGravity(Gravity.CENTER);
+        
+        ImageView imageView = new ImageView(getApplicationContext());
+        imageView.setLayoutParams(new LayoutParams(220, 220));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        
+        if (drawable != null) {
+        	imageView.setImageDrawable(drawable);
+        }
+        layout.addView(imageView);
+        return layout;
+       }
 
 
     @Override
