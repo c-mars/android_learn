63f7366867ec2747abd8b364ecebfc1ba4c55309
mars <constantine.mars@gmail.com>
2014-06-17 13:45:28 +0300

    ImageLoader class with caching logic implemented

--- ImageCachingExample/src/c/mars/imagecachingex/MainActivity.java
+++ ImageCachingExample/src/c/mars/imagecachingex/MainActivity.java
@@ -1,10 +1,13 @@
 package c.mars.imagecachingex;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.Fragment;
@@ -14,10 +17,12 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -28,10 +33,11 @@ public class MainActivity extends ActionBarActivity {
 	final static String landscapesFolder = "landscapes";
 	static String[] files;
 	static AssetManager assets;
-	
 	ImageView imageView;  
 	LinearLayout gallery;
-	ImageAdapter adapter;
+	private ImageLoader imgLoader;
+	Button button;
+	String fileName;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,30 +45,33 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         
         imageView = (ImageView)findViewById(R.id.imageView);
-        gallery = (LinearLayout)findViewById(R.id.gallery);
-        adapter = new ImageAdapter(this);
-        
+        gallery = (LinearLayout)findViewById(R.id.gallery);        
         assets = getAssets();
+        imgLoader = new ImageLoader(this);
+        button = (Button)findViewById(R.id.button);
+        
         try {
 			files = assets.list(landscapesFolder);
-		int i = 0;
-            for (String file : files) {
-            	loadImage(file);
-//            	return;
-            }
-				
+			if (files.length > 0) {
+				fileName = landscapesFolder + "/" +  files[0];
+				imgLoader.DisplayImage(fileName, imageView);
+			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
         
-//        String ExternalStorageDirectoryPath = Environment
-//                .getExternalStorageDirectory()
-//                .getAbsolutePath();
-//              
-//              String targetPath = ExternalStorageDirectoryPath + "/test/";
-//              Toast.makeText(getApplicationContext(), targetPath, Toast.LENGTH_LONG).show();
-              
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (files.length > 0) {
+					imgLoader.DisplayImage(landscapesFolder + "/" +  files[0], imageView); 
+				} else {
+					Toast.makeText(getApplicationContext(), "There are no asset images", Toast.LENGTH_SHORT);
+				}
+			}
+		});
     }
     
     void loadImage(String file) throws IOException {
@@ -79,30 +88,9 @@ public class MainActivity extends ActionBarActivity {
 
 		Drawable drawable = Drawable.createFromStream(stream, null);
 		if (drawable != null) {
-//			imageView.setImageDrawable(drawable);	
-			gallery.addView(insertPhoto(drawable));
+			imageView.setImageDrawable(drawable);	
 		}
-
     }
-    
-    View insertPhoto(Drawable drawable){
-//        Bitmap bm = decodeSampledBitmapFromUri(path, 220, 220);
-        
-        LinearLayout layout = new LinearLayout(getApplicationContext());
-        layout.setLayoutParams(new LayoutParams(250, 250));
-        layout.setGravity(Gravity.CENTER);
-        
-        ImageView imageView = new ImageView(getApplicationContext());
-        imageView.setLayoutParams(new LayoutParams(220, 220));
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        
-        if (drawable != null) {
-        	imageView.setImageDrawable(drawable);
-        }
-        layout.addView(imageView);
-        return layout;
-       }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -123,21 +111,4 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-    	
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
 }
